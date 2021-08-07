d69ab53219d5cc4da2cc6c1bba356ce9ee113812
Justin Elmore <jelmore1674@pm.me>
2021-08-07 17:27:09 -0500

    updated prod-env variable

--- next.config.js
+++ next.config.js
@@ -1,6 +1,3 @@
 module.exports = {
     reactStrictMode: true,
-    env: {
-        MONGO_URI: 'mongodb+srv://jelmore1674:bPRsEIwL5gmhbHqY@cluster0.gas9o.mongodb.net/justinBlog?retryWrites=true&w=majority',
-    },
 };
